5d4ab010371af863a306b74427b4820d773086e1
Gabriel Bordenabe <gabrielbordenabe@gmail.com>
2024-02-20 10:12:04 -0300

    Add translate use case and response interfaces

---
+++ src/core/use-cases/translate.use-case.ts
@@ -0,0 +1,27 @@
+import { TranslateResponse } from '../../interfaces';
+
+export const translateUseCase = async (prompt: string, lang: string) => {
+  try {
+    const resp = await fetch(`${import.meta.env.VITE_GPT_API}/translate`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ prompt, lang }),
+    })
+    
+    if (!resp.ok) throw new Error('No se pudo realizar la traduccion')
+    const data = await resp.json() as TranslateResponse
+
+    return {
+      ok: true,
+      content: data.content
+    }
+    
+  } catch (error) {
+    return {
+      ok: false,
+      message: ''
+    }
+  }
+}
--- src/presentation/pages/translate/TranslatePage.tsx
+++ src/presentation/pages/translate/TranslatePage.tsx
@@ -1,3 +1,73 @@
+import { useState } from 'react';
+import { GptMessage, MyMessage, TypingLoader, TextMessageBoxSelect } from '../../components';
+import { translateUseCase } from '../../../core/use-cases';
+
+
+interface Message{
+  text: string
+  isGpt: boolean
+}
+
+const languages = [
+  { id: "alemán", text: "Alemán" },
+  { id: "árabe", text: "Árabe" },
+  { id: "bengalí", text: "Bengalí" },
+  { id: "francés", text: "Francés" },
+  { id: "hindi", text: "Hindi" },
+  { id: "inglés", text: "Inglés" },
+  { id: "japonés", text: "Japonés" },
+  { id: "mandarín", text: "Mandarín" },
+  { id: "portugués", text: "Portugués" },
+  { id: "ruso", text: "Ruso" },
+];
+
 export const TranslatePage = () => {
-  return <div>TranslatePage</div>;
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const handlePost = async (text:string, selecterOption:string) => {
+    setIsLoading(true)
+
+    const newMessage = `Traduce: "${text}" al idioma ${selecterOption}`
+    setMessages(prev => [...prev, {text: newMessage, isGpt: false}])
+
+    const {ok, content} = await translateUseCase(text, selecterOption)
+    setIsLoading(false)
+
+    if(!ok) return
+    setMessages(prev => [...prev, {text: content || '', isGpt: true}])
+  }
+
+
+  return (
+    <div className='chat-container'>
+      <div className='chat-messages'>
+        <div className='grid grid-cols-12 gap-y-2'>
+          <GptMessage text='¿Que quieres que traduzca hoy?' />
+          {
+            messages.map((message, index) => (
+              message.isGpt 
+                ? <GptMessage key={index} text={message.text} />
+                : <MyMessage key={index} text={message.text} />
+            ))
+          }
+
+          {
+            isLoading && (
+              <div className='col-start-1 col-end-12 flex justify-center'>
+                <TypingLoader/>
+              </div>
+            )
+          }
+        </div>
+      </div>
+
+      <TextMessageBoxSelect
+        onSendMessage={handlePost}
+        placeholder='Escribe tu texto aquí'
+        options={languages}
+      />
+    </div>
+  );
 };
