a1927229308c6d1aa18d98aa4ad560cb1c8722a0
Gabriel Bordenabe <gabrielbordenabe@gmail.com>
2024-03-28 12:44:59 -0300

    Add assistance response interface and use cases

--- src/presentation/pages/assistant/AssistantPage.tsx
+++ src/presentation/pages/assistant/AssistantPage.tsx
@@ -1,3 +1,94 @@
+import { useEffect, useState } from 'react';
+import {
+  GptMessage,
+  MyMessage,
+  TypingLoader,
+  TextMessageBox,
+} from '../../components';
+import {
+  createThreadUseCase,
+  postQuestionUseCase,
+} from '../../../core/use-cases';
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
 export const AssistantPage = () => {
-  return <div>AssistantPage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [threadId, setThreadId] = useState<string>();
+
+  useEffect(() => {
+    const threadId = localStorage.getItem('threadId');
+    if (threadId) {
+      setThreadId(threadId);
+    } else {
+      createThreadUseCase().then((threadId) => {
+        setThreadId(threadId);
+        localStorage.setItem('threadId', threadId);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (threadId) {
+      setMessages((prev) => [
+        ...prev,
+        { text: 'Thead ID: ' + threadId, isGpt: true },
+      ]);
+    }
+  }, [threadId]);
+
+  const handlePost = async (text: string) => {
+    if (!threadId) return;
+
+    setIsLoading(true);
+    setMessages([...messages, { text, isGpt: false }]);
+
+    const replies = await postQuestionUseCase(threadId, text);
+
+    setMessages([]);
+
+    setIsLoading(false);
+
+    for (const reply of replies) {
+      for (const message of reply.content) {
+        setMessages((prev) => [
+          ...prev,
+          { text: message, isGpt: reply.role === 'assistant' },
+        ]);
+      }
+    }
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          <GptMessage text="Buen dia, soy Sam, en que puedo ayudarte?" />
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text={message.text} />
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            ),
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 flex justify-center">
+              <TypingLoader />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe tu texto aquí"
+        disableCorrections
+      />
+    </div>
+  );
 };
